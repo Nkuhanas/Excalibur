2185fe86d85086e0c0299bcec7c74418b9fe2ce9
Nkuhanas <105261172+Nkuhanas@users.noreply.github.com>
2022-10-21 21:53:53 -0700

    Update data.lua

--- data.lua
+++ data.lua
@@ -1,1 +1,25 @@
-return "burger"
+local data = {}
+
+data.Resetters = {
+	{"Black Dwarf", "Void Star"},
+	{"Tesla Resetter", "Tesla Refuter"},
+	{"Daestrophe"},
+	{"The Ultimate Sacrifice", "The Final Upgrader"}
+}
+
+data.Boxes = {
+	"Inferno",
+	"Regular",
+	"Unreal",
+	"Birthday",
+}
+
+data.Types = {
+	Upgrade = Upgraders,
+	Lava = Furnaces,
+	Cannon = Upgraders,
+	Scan = Upgraders,
+	Drop = Droppers,
+}
+
+return data
