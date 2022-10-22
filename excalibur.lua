cf6cd08f610036258b205504d49cebb89a4fcb65
Nkuhanas <105261172+Nkuhanas@users.noreply.github.com>
2022-10-22 11:04:48 -0700

    Update excalibur.lua

--- excalibur.lua
+++ excalibur.lua
@@ -3,10 +3,10 @@ if game.PlaceId ~= 258258996 then return end
 
 --// EXCALIBUR INITIALIZATION
 
-getgenv().Excalibur = {}
-getgenv().Excalibur.Data = loadstring(game:HttpGet("https://raw.githubusercontent.com/Nkuhanas/Excalibur/main/data.lua"))()
-getgenv().Excalibur.Items = loadstring(game:HttpGet("https://raw.githubusercontent.com/Nkuhanas/Excalibur/main/items.lua"))()
-getgenv().Excalibur.Automation = loadstring(game:HttpGet("https://raw.githubusercontent.com/Nkuhanas/Excalibur/main/automation.lua"))()
+local Excalibur = {}
+Excalibur.Data = loadstring(game:HttpGet("https://raw.githubusercontent.com/Nkuhanas/Excalibur/main/data.lua"))()
+Excalibur.Items = loadstring(game:HttpGet("https://raw.githubusercontent.com/Nkuhanas/Excalibur/main/items.lua"))(Excalibur.Data)
+Excalibur.Automation = loadstring(game:HttpGet("https://raw.githubusercontent.com/Nkuhanas/Excalibur/main/automation.lua"))(Excalibur.Data, Excalibur.Items)
 
 --// SERVICES
 
